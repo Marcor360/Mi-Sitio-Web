15dea1df6d2d7df8990e81bc182b48d56258a584
Marco Antonio Rulfo Castro <81320236+Marcor360@users.noreply.github.com>
2022-08-23 22:04:44 -0500

    Creacion y Carga de Repositorio
    Se crea y se carga el repositorio del Proyecto, se agregaron las carpetas de CSS, JS, IMG, se subio el index.html y el index.php, recordar que la carpeta esta en php por lo cual hay que descargarlo en el servidor local para ejecutarlo

---
+++ Nav.php
@@ -0,0 +1,10 @@
+<div class="nav-bg">
+<nav class="Navegacion-Principal Contenedor">
+        <a href="index.php">Inicio</a>
+        <a href="#">¿Que es MR360web?</a>
+        <a href="#">¿Quien soy?</a>
+        <a href="#">Etudios</a>
+        <a href="#">Precios</a>
+        <a href="#">Contactame</a>
+</nav>
+</div>
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Normalize.css"><!--- Normalize --->
+    <link rel="preload" href="css/index.css" as="style">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="Titulo-pagina">
+    <h1>MR<span class="spn-1">360web</span><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-laptop" width="60" height="60" viewBox="0 0 24 24" stroke-width="2" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <line x1="3" y1="19" x2="21" y2="19" />
+  <rect x="5" y="6" width="14" height="10" rx="1" />
+</svg></h1>
+    </div>
+    <!-- Inicio Nav -->
+    <?php include('Nav.php');?><!--- Navbar --->
+    <!-- Fin Nav -->
+    <section class="hero"> <!--- contenido del titulo -->
+    <div class="Dev-hero"> <!--- Contenido del titulo hijo --->
+      <h2 class="DS">Desarollo<span> web</span></h2>
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-laptop" width="44" height="44" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <line x1="3" y1="19" x2="21" y2="19" />
+  <rect x="5" y="6" width="14" height="10" rx="1" />
+</svg> <!--- SVG Laptop acompañante del titulo --->
+    </div>
+    </section>
+    <div class="Q-y-Q">
+        <div class="Que-es-mr">
+            <h2>MR360web</h2>
+            <p> Es un pequeño negocio que se dedica a la creacion,
+                desarollo y mantenimientos a sitios web, tambien 
+                nos dedicamos a administrar las redes sociales de los 
+                clientes, para de esta manera facilitar y agilizar el trabajo
+                de las personas o empresas. 
+            </p>
+        </div>
+        <div class="Quien-soy">
+            <h2>¿Quien soy?</h2>
+            <p>Hola me presento mi nombre es "Marco Antonio" y soy el creador de MR360web y el que se encargara de desarrolar
+               tu pagina web.
+            </p>
+        </div>
+    </div>
+    <div class="E-y-P">
+        <div class="Estudios">
+          <h2>Estudios</h2>
+          <p>Actualmente estoy estudiando la carrera de "Ingeneria en Sistemas" y tengo un titulo en Informatica y Certificaciones</p>
+        </div>
+        <div class="Precios">
+          <h3>Precios</h3>
+          <p>Los precios dependeran del proyectos que tu desees
+            si es asite invito a que tepongas en contacto con migo 
+            para poider darte el mejor precio posible dependiendo de 
+            tu proyecto
+          </p>
+        </div>
+    </div>
+    <div class="Conct">
+        <div class="Contacto">
+          <h3>Contacto</h3>
+          <a href="mailto:marcorulfo100@gmail.com?body=Hola%20Marco%2C%20quisiera%20saber%20m%C3%A1s%20informaci%C3%B3n%20acerca%20del%20desarrollo%20web%20de%20un%20sitio%F0%9F%98%8A%F0%9F%92%BB">Email</a>
+          <a href="https://wa.me/525531083353?text=Hola,%20quiero%20mas%20información%20hacerca%20de%20tus%20servicios%20de%20desarrollo%20web%20😊💻">Whats App</a>
+        </div>
+    </div>
+</body>
+</html>
